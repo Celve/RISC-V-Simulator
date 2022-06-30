0f09a243f68d29b5585ea036607cc6352f791c2a
Celve <celve03@gmail.com>
2022-06-30 09:49:40 +0800

    fix(branch-predictor): fix bugs with two level adaptive predictor

--- src/prediction/branch_predictor.cpp
+++ src/prediction/branch_predictor.cpp
@@ -1,19 +1,25 @@
 #include "prediction/branch_predictor.h"
 
+#include <cstring>
+
 namespace riscv {
 
 BranchPredictor::BranchPredictor() {
-  for (int i = 0; i < MEMORY_SIZE; i++) {
-    counter_[i] = 0;
-    history_[i] = 0;
-  }
+  memset(counter_, 0, sizeof(counter_));
+  memset(history_, 0, sizeof(history_));
 }
 
 bool BranchPredictor::Predict(u32 addr) { return (counter_[addr + history_[addr]] & 2) != 0; }
 
 void BranchPredictor::Feedback(u32 addr, bool is_taken) {
-  counter_[addr + history_[addr]] = (counter_[addr + history_[addr]] << 1) | int(is_taken);
-  counter_[addr + history_[addr]] = (counter_[addr + history_[addr]] << 1);
+  auto real_addr = addr + history_[addr];
+  if (is_taken) {
+    counter_[real_addr] = (counter_[real_addr] == 3 ? 3 : counter_[real_addr] + 1);
+  } else {
+    counter_[real_addr] = (counter_[real_addr] == 0 ? 0 : counter_[real_addr] - 1);
+  }
+  history_[addr] = (history_[addr] << 1) | int(is_taken);
+  history_[addr] &= 3;
 }
 
 }  // namespace riscv
